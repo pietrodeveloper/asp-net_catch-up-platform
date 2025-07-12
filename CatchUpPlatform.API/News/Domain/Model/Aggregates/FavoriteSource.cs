550e5682121b4397d840fee99284dd119e21b1a1
Pietro Minaya <pietrominaya@Pietros-Laptop.local>
2025-07-12 14:35:44 -0500

    chore: initial commit

---
+++ CatchUpPlatform.API/News/Domain/Model/Aggregates/FavoriteSource.cs
@@ -0,0 +1,16 @@
+using CatchUpPlatform.API.News.Domain.Model.Commands;
+
+namespace CatchUpPlatform.API.News.Domain.Model.Aggregates;
+
+public class FavoriteSource
+{
+    public int Id { get; set; }
+    public string NewsApiKey { get; set; }
+    public string SourceId { get; set; }
+
+    public FavoriteSource(CreateFavoriteSourceCommand command)
+    {
+        this.NewsApiKey = command.NewsApiKey;
+        this.SourceId = command.SourceId;
+    }
+}
