22648fcc6426d0b9b18c247dce1abbdd98238f59
Pietro Minaya <pietrominaya@Pietros-Laptop.local>
2025-07-19 21:21:29 -0500

    chore: implemented base classes for repository and unit of work

--- CatchUpPlatform.API/Shared/Domain/Repositories/IBaseRepository.cs
+++ CatchUpPlatform.API/Shared/Domain/Repositories/IBaseRepository.cs
@@ -3,7 +3,7 @@ namespace CatchUpPlatform.API.Shared.Domain.Repositories;
 public interface IBaseRepository<TEntity>
 {
     Task AddAsync(TEntity entity);
-    Task<TEntity> FindByIdAsync(int id);
+    Task<TEntity?> FindByIdAsync(int id);
     void Update(TEntity entity);
     void Remove(TEntity entity);
     Task<IEnumerable<TEntity>> ListAsync();
